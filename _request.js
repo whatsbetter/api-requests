3ad85eafba72e2e4348dbf724d798c56bf96bfcc
kovalit <kovalit@mail.ru>
2018-05-30 16:32:00 +0300

    fix(request): optimization

--- _request.js
+++ _request.js
@@ -1,25 +1,9 @@
 import { GraphQLClient } from 'graphql-request'
 import camelize from 'camelize';
-
 const client = new GraphQLClient('https://api2.toprater.com/graphql');
-
-const request = (query) => {
-    let token = getCookie("token");
-
-    if (token) {
-	client.setHeaders({"X-Token" : token});
-    }
-
-    console.log(query);
-    return client
-	.request(query)
-	.then(body => camelize(body));
-};
-
 
 const getCookie = (name) => {
     let matches;
-    
     matches = document.cookie.match(new RegExp('(?:^|; )' + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + '=([^;]*)'));
     
     if (matches) {
@@ -30,5 +14,16 @@ const getCookie = (name) => {
     }
 };
 
+let token = getCookie("token");
+if (token) {
+    client.setHeaders({"X-Token" : token});
+}
+
+const request = (query) => {
+    console.log(query);
+    return client
+	.request(query)
+	.then(body => camelize(body));
+};
 
 export default request;
