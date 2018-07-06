44452a2a444d13c283624a3ba2f2c4045ee44443
kovalit <kovalit@mail.ru>
2018-07-06 18:37:16 +0300

    fix: user

--- users.js
+++ users.js
@@ -19,7 +19,7 @@ export function search(params) {
  * @returns {Function} 
  */
 export function findMe(params) {
-    let query =`{user (token: "${params.token}"){id,name,first_name,second_name,main_image,karma,subscription_spheres{id,label,name}}}`;
+    let query =`{user (token: "${params.token}"){id,name,first_name,second_name,main_image,karma,providers{type},subscription_spheres{id,label,name}}}`;
     return request(query);
 }
 
