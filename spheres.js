98a168ea189def764a05c64b589ba753c2b7db21
maxim <mymailforxperia@gmail.com>
2018-10-04 10:08:55 +0300

    cities

--- spheres.js
+++ spheres.js
@@ -40,7 +40,7 @@ export function findAll(params) {
  * @returns {Function}
  */
 export function findById(params) {
-    let query = `{sphere(id: "${params.id}"){name,id,description,label,count_criteria,count_entities,count_scores,popular_criteria{name,id,label}}}`;
+    let query = `{sphere(id: "${params.id}"){name,id,description,label,count_criteria,count_entities,have_map,count_scores,popular_criteria{name,id,label}}}`;
     return request(query);
 }
 
