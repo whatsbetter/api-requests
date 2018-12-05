db53673449ba0d14d675792fe6876c6f78f06ada
kovalit <kovalit@mail.ru>
2018-12-05 18:55:45 +0300

    fix (feed)

--- feed.js
+++ feed.js
@@ -10,6 +10,6 @@ import request from './_request';
 
 export function get(params) {  
     let conditions = getConditions(params);  
-    let query = `{feed{created_at,end,start,sphere{id,name,label},scores{id,criterion{id,label},count_sub_comments,comment{id,text,useful,useless},entity{id,label,main_image},value},user{id,name,main_image,karma}}}`;
+    let query = `{feed{created_at,end,start,sphere{id,name,label},scores{id,criterion{id,label},count_sub_comments,comment{id,text,updated_at,useful,useless},entity{id,label,main_image},value},user{id,name,main_image,karma}}}`;
     return request(query);
 }
